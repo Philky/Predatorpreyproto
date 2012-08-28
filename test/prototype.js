609628cca09b600d65c1b83a6b75df7cedbf4092
Phil Combridge <philky@philky.net>
2012-08-28 21:53:14 +1000

    Fail auto test

--- test/prototype.js
+++ test/prototype.js
@@ -1,7 +1,13 @@
 var prototype = require('../lib/prototype');
 
 // exports.testInitCreatesThreeLives = function(test) {
-  // var result = prototype.Game.init();
-  // test.equal(prototype.Game.life.length, 3, "init should create 3 lives");
-  // test.done();
+//   var result = prototype.Game.init();
+//   test.equal(prototype.Game.life.length, 3, "init should create 3 lives");
+//   test.done();
+// };
+
+// exports.testResetClearsLife = function(test) {
+//   var result = prototype.Game.reset();
+//   test.equal(prototype.Game.life.length, 0, "reset should set life length to zero");
+//   test.done();
 // };
